c8e4dbb8366e0b2d8d19b71d3dbb10ea610e6ebb
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2019-10-11 10:48:44 -0400

    vuforia risk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables/Reference.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables/Reference.java
@@ -10,5 +10,9 @@ public class Reference {
     //Servo constant
     public static final double servoDegreesConst = 0.005d; //degrees to power
 
-    //Servo Starting P
+    //Servo Starting Positions
+    
+    
+    //Current Vuforia AI risk factor
+    public static final String vuforiaRisk = "LOW-MEDIUM";
 }
