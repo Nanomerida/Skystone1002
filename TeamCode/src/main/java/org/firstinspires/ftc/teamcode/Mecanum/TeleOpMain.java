c04c94dccc5531d83818367a865e81dc368ea829
Nanomerida <orlandito@thekarams.net>
2019-10-28 16:41:39 -0400

    Start TeleOp for Mechanum
    hopefully none of those matrix indices are mixed up...
    
    HOLA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/TeleOpMain.java
@@ -1,15 +1,27 @@
 package org.firstinspires.ftc.teamcode.Mecanum;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.hardwareMaps.HardwareMapMain;
 
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOpMain", group="TeleOp")
+@TeleOp(name = "TeleOpMain", group="TeleOp")
 
 public class TeleOpMain extends OpMode {
+
+
+    public DcMotor left_front_drive = null;
+    public DcMotor left_back_drive = null;
+    public DcMotor right_front_drive = null;
+    public DcMotor right_back_drive = null;
+    public DcMotor lift_motor = null;
+
+
+
+
+
     //Crates HardwareMap object robot
-    HardwareMapMain robot = new HardwareMapMain();
 
     public float[] m_v_mult(float[][] m, float[] v) {
         float[] out = new float[4];
@@ -23,7 +35,22 @@ public class TeleOpMain extends OpMode {
     //Initializes with the hardwareMap
     @Override
     public void init() {
-        robot.init(hardwareMap);
+
+        left_front_drive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        left_back_drive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        right_front_drive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        right_back_drive = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+        lift_motor = hardwareMap.get(DcMotor.class, "lift_motor");
+
+
+        left_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        lift_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
 
     }
 
@@ -37,6 +64,15 @@ public class TeleOpMain extends OpMode {
 
     public void loop() {
 
+        if (gamepad2.right_bumper) {
+            lift_motor.setPower(0.5);
+        }
+        else if (gamepad2.left_bumper) {
+            lift_motor.setPower(-0.5);
+        } else {
+            lift_motor.setPower(0);
+        }
+
 
         float[] inputs = {gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x};
         float[] outputs;
@@ -49,10 +85,10 @@ public class TeleOpMain extends OpMode {
 
         outputs = m_v_mult(matrix, inputs);
 
-        robot.left_front_drive.setPower(outputs[2]);
-        robot.left_back_drive.setPower(outputs[3]);
-        robot.right_front_drive.setPower(outputs[0]);
-        robot.right_back_drive.setPower(outputs[1]);
+        left_front_drive.setPower(outputs[3]);
+        left_back_drive.setPower(outputs[2]);
+        right_front_drive.setPower(outputs[0]);
+        right_back_drive.setPower(outputs[1]);
 
 
 
