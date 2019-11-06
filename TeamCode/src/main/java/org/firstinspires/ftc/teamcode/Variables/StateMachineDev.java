7187c900ff1a68da44fc162000e47725549ab2e3
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2019-11-06 11:49:44 -0500

    Sasfas

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables/StateMachineDev.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Variables;
+
+import org.firstinspires.ftc.robotcore.external.StateMachine;
+import org.firstinspires.ftc.robotcore.external.StateTransition;
+
+
+public class StateMachineDev {
+
+public StateMachineDev() {
+
+}
+
+StateMachine() stateMachine = new StateMachine();
+
+stateMachine.addTransition(new StateTransition(State HI, Event T, State G));
+
+
+
+
+
+
+
+
+}
