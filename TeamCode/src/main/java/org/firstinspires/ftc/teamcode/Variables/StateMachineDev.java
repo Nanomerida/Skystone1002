a1d6afd0726d2c699185344369c105752fbb9966
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2019-11-06 12:03:53 -0500

    asda

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables/StateMachineDev.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables/StateMachineDev.java
@@ -2,9 +2,17 @@ package org.firstinspires.ftc.teamcode.Variables;
 
 import org.firstinspires.ftc.robotcore.external.StateMachine;
 import org.firstinspires.ftc.robotcore.external.StateTransition;
+import org.firstinspires.ftc.robotcore.external.State;
+import org.firstinspires.ftc.robotcore.external.Event;
 
 
-public class StateMachineDev {
+public class StateMachineDev implements State, Event{
+  
+private enum State implements Event {
+  T,
+  G,
+  J
+}
 
 public StateMachineDev() {
 
