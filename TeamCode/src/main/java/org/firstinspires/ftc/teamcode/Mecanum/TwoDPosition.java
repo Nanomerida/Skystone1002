21f9e212754d2cc2fe77c03921075fc80499a5f4
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2019-11-20 13:30:18 -0500

    dads

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/TwoDPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/TwoDPosition.java
@@ -1,16 +1,13 @@
 package org.firstinspires.ftc.teamcode.Mecanum;
 
-import org.firstinspires.ftc.external.navigation.Positon;
-import org.firstinspires.ftc.external.navigation.DistanceUnit;
 
-
-public class 2dPosition {
+public class TwoDPosition {
 
   double x;
   double y;
   final long timeStamp;
 
-  public 2dPosition(double x, double y) {
+  public TwoDPosition(double x, double y) {
     this.timeStamp = System.nanoTime();
     this.x = x;
     this.y = y;
