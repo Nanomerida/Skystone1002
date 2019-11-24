12cf44d38f6658ab6191e9e2eee8f32b872db7b2
Nelitha <1234legobrick@gmail.com>
2019-11-24 17:22:14 -0500

    IT  WORKS!!!!!!!!!!!!!!!!!!!!!!!!!
    PRAY TO THE FTC GODS

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/LastResortAuton.java
@@ -0,0 +1,460 @@
+package org.firstinspires.ftc.teamcode.Mecanum;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.CRVuforia.VuforiaBlue;
+import org.firstinspires.ftc.teamcode.Methods.GeneralMethods;
+
+@Autonomous(name="Last Resort", group = "Autonomous")
+public class LastResortAuton extends LinearOpMode {
+
+    VuforiaBlue blockPosBlue = new VuforiaBlue(); //creates an instance of the vuforia blue side file
+    GeneralMethods methods = new GeneralMethods();
+    private ElapsedTime refreshTimer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
+    DcMotor left_front_drive = null;
+    DcMotor left_back_drive = null;
+    DcMotor right_front_drive = null;
+    DcMotor right_back_drive = null;
+    DcMotor arm = null;
+    CRServo claw = null;
+    public WebcamName webcam = null;
+
+    //public BNO055IMU imu;
+    //private Orientation angles;
+
+    @Override
+    public void runOpMode() {
+
+
+        left_front_drive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        left_back_drive = hardwareMap.get(DcMotor.class, "left_back_drive");
+        right_front_drive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        right_back_drive = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+        arm = hardwareMap.get(DcMotor.class, "arm");
+        webcam = hardwareMap.get(WebcamName.class, "Webcam 1");
+        claw = hardwareMap.get(CRServo.class, "claw");
+
+        blockPosBlue.blueInit(webcam);
+
+
+        left_front_drive.setDirection(DcMotor.Direction.REVERSE);
+        left_back_drive.setDirection(DcMotor.Direction.REVERSE);
+
+
+/*
+to strafe (right?):
+        left_front_drive.setPower(-0.6);
+        left_back_drive.setPower(0.9);
+        right_front_drive.setPower(0.6);
+        right_back_drive.setPower(-0.9);
+*/
+
+        waitForStart();
+
+
+        left_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------
+        /**This picks up a random stone and drops it off. Then parks*/
+        /*
+        left_front_drive.setPower(0.3);
+        left_back_drive.setPower(0.3);
+        right_front_drive.setPower(0.3);
+        right_back_drive.setPower(0.3);
+
+        sleep(1800);
+
+        left_front_drive.setPower(0);
+        left_back_drive.setPower(0);
+        right_front_drive.setPower(0);
+        right_back_drive.setPower(0);
+
+
+        claw.setPower(-0.3);
+        sleep(3000);
+        claw.setPower(-0.1);
+
+        sleep(500);
+
+        left_front_drive.setPower(-0.3);
+        left_back_drive.setPower(-0.3);
+        right_front_drive.setPower(-0.3);
+        right_back_drive.setPower(-0.3);
+
+        sleep(1300);
+
+        left_front_drive.setPower(0);
+        left_back_drive.setPower(0);
+        right_front_drive.setPower(0);
+        right_back_drive.setPower(0);
+
+        sleep(500);
+
+        left_front_drive.setPower(0.6);
+        left_back_drive.setPower(-0.9);
+        right_front_drive.setPower(-0.6);
+        right_back_drive.setPower(0.9);
+
+        sleep(1800);
+
+        left_front_drive.setPower(0);
+        left_back_drive.setPower(0);
+        right_front_drive.setPower(0);
+        right_back_drive.setPower(0);
+
+        sleep(500);
+
+        claw.setPower(0.3);
+        sleep(2000);
+        claw.setPower(0);
+
+        sleep(500);
+
+        left_front_drive.setPower(-0.6);
+        left_back_drive.setPower(0.3);
+        right_front_drive.setPower(0.2);
+        right_back_drive.setPower(-0.3);
+
+        sleep(1100);
+
+        left_front_drive.setPower(0);
+        left_back_drive.setPower(0);
+        right_front_drive.setPower(0);
+        right_back_drive.setPower(0);
+
+        sleep(500);
+
+        left_front_drive.setPower(0.3);
+        left_back_drive.setPower(0.3);
+        right_front_drive.setPower(0.3);
+        right_back_drive.setPower(0.3);
+
+        sleep(1300);
+
+        left_front_drive.setPower(0);
+        left_back_drive.setPower(0);
+        right_front_drive.setPower(0);
+        right_back_drive.setPower(0);
+
+
+
+        idle();
+        */
+// ---------------------------------------------------------------------------------------------------------------------------
+
+
+
+        left_front_drive.setPower(0.3);
+        left_back_drive.setPower(0.3);
+        right_front_drive.setPower(0.3);
+        right_back_drive.setPower(0.3);
+
+        sleep(750);
+
+        left_front_drive.setPower(0);
+        left_back_drive.setPower(0);
+        right_front_drive.setPower(0);
+        right_back_drive.setPower(0);
+
+
+        int skystonePos = blockPosBlue.visionTest();
+
+        switch(skystonePos){
+            case 0://left
+                left_front_drive.setPower(-0.6);
+                left_back_drive.setPower(0.9);
+                right_front_drive.setPower(0.6);
+                right_back_drive.setPower(-0.9);
+
+                sleep(200);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                left_front_drive.setPower(0.3);
+                left_back_drive.setPower(0.3);
+                right_front_drive.setPower(0.3);
+                right_back_drive.setPower(0.3);
+
+                sleep(1000);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+
+                claw.setPower(-0.3);
+                sleep(3000);
+                claw.setPower(-0.1);
+
+                sleep(500);
+
+                left_front_drive.setPower(-0.3);
+                left_back_drive.setPower(-0.3);
+                right_front_drive.setPower(-0.3);
+                right_back_drive.setPower(-0.3);
+
+                sleep(1300);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                left_front_drive.setPower(0.6);
+                left_back_drive.setPower(-0.9);
+                right_front_drive.setPower(-0.6);
+                right_back_drive.setPower(0.9);
+
+                sleep(1800);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                claw.setPower(0.3);
+                sleep(2000);
+                claw.setPower(0);
+
+                sleep(500);
+
+                left_front_drive.setPower(-0.6);
+                left_back_drive.setPower(0.3);
+                right_front_drive.setPower(0.2);
+                right_back_drive.setPower(-0.3);
+
+                sleep(1100);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                left_front_drive.setPower(0.3);
+                left_back_drive.setPower(0.3);
+                right_front_drive.setPower(0.3);
+                right_back_drive.setPower(0.3);
+
+                sleep(1300);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+                break;
+
+            case 1://middle
+                left_front_drive.setPower(0.3);
+                left_back_drive.setPower(0.3);
+                right_front_drive.setPower(0.3);
+                right_back_drive.setPower(0.3);
+
+                sleep(1200);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+
+                claw.setPower(-0.3);
+                sleep(3000);
+                claw.setPower(-0.1);
+
+                sleep(500);
+
+                left_front_drive.setPower(-0.3);
+                left_back_drive.setPower(-0.3);
+                right_front_drive.setPower(-0.3);
+                right_back_drive.setPower(-0.3);
+
+                sleep(1300);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                left_front_drive.setPower(0.6);
+                left_back_drive.setPower(-0.9);
+                right_front_drive.setPower(-0.6);
+                right_back_drive.setPower(0.9);
+
+                sleep(1800);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                claw.setPower(0.3);
+                sleep(2000);
+                claw.setPower(0);
+
+                sleep(500);
+
+                left_front_drive.setPower(-0.6);
+                left_back_drive.setPower(0.3);
+                right_front_drive.setPower(0.2);
+                right_back_drive.setPower(-0.3);
+
+                sleep(1100);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                left_front_drive.setPower(0.3);
+                left_back_drive.setPower(0.3);
+                right_front_drive.setPower(0.3);
+                right_back_drive.setPower(0.3);
+
+                sleep(1300);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                break;
+
+            case 2://right
+
+                left_front_drive.setPower(0.6);
+                left_back_drive.setPower(-0.9);
+                right_front_drive.setPower(-0.6);
+                right_back_drive.setPower(0.9);
+
+                sleep(300);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                left_front_drive.setPower(0.3);
+                left_back_drive.setPower(0.3);
+                right_front_drive.setPower(0.3);
+                right_back_drive.setPower(0.3);
+
+                sleep(1000);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+
+                claw.setPower(-0.3);
+                sleep(3000);
+                claw.setPower(-0.1);
+
+                sleep(500);
+
+                left_front_drive.setPower(-0.3);
+                left_back_drive.setPower(-0.3);
+                right_front_drive.setPower(-0.3);
+                right_back_drive.setPower(-0.3);
+
+                sleep(1300);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                left_front_drive.setPower(0.6);
+                left_back_drive.setPower(-0.9);
+                right_front_drive.setPower(-0.6);
+                right_back_drive.setPower(0.9);
+
+                sleep(1800);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                claw.setPower(0.3);
+                sleep(2000);
+                claw.setPower(0);
+
+                sleep(500);
+
+                left_front_drive.setPower(-0.6);
+                left_back_drive.setPower(0.3);
+                right_front_drive.setPower(0.2);
+                right_back_drive.setPower(-0.3);
+
+                sleep(1100);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+
+                left_front_drive.setPower(0.3);
+                left_back_drive.setPower(0.3);
+                right_front_drive.setPower(0.3);
+                right_back_drive.setPower(0.3);
+
+                sleep(1300);
+
+                left_front_drive.setPower(0);
+                left_back_drive.setPower(0);
+                right_front_drive.setPower(0);
+                right_back_drive.setPower(0);
+
+                sleep(500);
+                break;
+        }
+    }
+}
