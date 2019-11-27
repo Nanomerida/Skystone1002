714afe96ac705c6d1a17e6a2901432f6e1a18ff7
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2019-11-26 19:10:54 -0500

    Deoihs0dihqwijpdbc

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/CRPosition/Globals.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Mecanum.CRPosition;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.internal.TelemetryImpl
+
+
+public class Globals {
+  //apparently  we can do this to access it globally
+  public static Telemetry telemetry = null;
+ 
+}
