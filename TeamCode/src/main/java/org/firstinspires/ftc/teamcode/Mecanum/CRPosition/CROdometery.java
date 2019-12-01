7c4e324b3558b19aa560949f800d7c1b03ad7066
Nelitha <1234legobrick@gmail.com>
2019-12-01 15:47:46 -0500

    updates
    HOLA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/CRPosition/CROdometery.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/CRPosition/CROdometery.java
@@ -10,6 +10,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import static java.lang.Math.PI;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
+import static java.lang.Math.abs;
+
+
 
 public class CROdometery {
 
@@ -27,9 +30,11 @@ public class CROdometery {
     public DcMotor  right_back_drive = null;
 
     private OpMode opMode;
+    public Moving move;
+    public Turning turn;
 
-/*    public CROdometery(OpMode opMode, ExpansionHubEx expansionHubEx, ExpansionHubMotor left_y_encoder, ExpansionHubMotor right_y_encoder, ExpansionHubMotor x_encoder,
-                       double[] start, double heading, ArrayList<DcMotor> driveMotors){
+    public CROdometery(OpMode opMode, ExpansionHubEx expansionHubEx, ExpansionHubMotor left_y_encoder, ExpansionHubMotor right_y_encoder, ExpansionHubMotor x_encoder,
+                       double[] start, double heading, DcMotor[] driveMotors){
 
         this.opMode = opMode;
         this.expansionHubEx = expansionHubEx;
@@ -37,39 +42,53 @@ public class CROdometery {
         this.right_y_encoder = new ExternalEncoder(right_y_encoder);
         this.x_encoder = new ExternalEncoder(x_encoder);
         this.robotPosition = new RobotPosition(start[0], start[1], heading, "Degrees");
-        this.left_front_drive = driveMotors.get(0);
-        this.left_back_drive = driveMotors.get(1);
-        this.right_front_drive = driveMotors.get(2);
-        this.right_back_drive = driveMotors.get(3);
+        this.left_front_drive = driveMotors[0];
+        this.left_back_drive = driveMotors[1];
+        this.right_front_drive = driveMotors[2];
+        this.right_back_drive = driveMotors[3];
         bulkData = this.expansionHubEx.getBulkInputData();
+        move = new Moving();
+        turn = new Turning();
     }
     
     
-     public void MoveOdomPosition(double GoalX, double GoalY){
+     public void MoveOdomPosition(double GoalX, double GoalY, double theta){
         boolean goalreachedPos;
         //Use odometry to move to a given position
         do {
             double[] previousPos = robotPosition.getPosition();
-            double[] actualPos = Movement.Moving.AbsolutePosition(previousPos[0], previousPos[1]);
-            goalReachedPos = Movement.Moving.GoalCheckPos(actualPos[0], GoalX, actualPos[1], GoalY); //check if we are at position
-            //if (!goalReachedPos) moveDrivebyPower(Movement.Moving.PositionChange(actualPos[0], GoalX, actualPos[2], GoalY));
+            double[] actualPos = move.AbsolutePosition(theta);
+            goalreachedPos = move.GoalCheckPos(actualPos[0], GoalX, actualPos[1], GoalY); //check if we are at position
+            if (!goalreachedPos) setDrivePower(move.PositionChange(actualPos[0], GoalX, actualPos[2], GoalY));
             previousPos[0] = actualPos[0];
             previousPos[1] = actualPos[1];
         }
-        while(!goalReachedPos);
-        stopDrive();
+        while(!goalreachedPos);
+        setDrivePower(new double[] {0,0,0,0});
+    }
+
+    private void MoveAngle(double thetaG, double thetaA){
+        boolean goalReachedAngle;
+        //Use odometry to rotate
+        do {
+            goalReachedAngle = turn.GoalCheckAngle(thetaG, thetaA); //check if we are at angle.
+            if (goalReachedAngle) {
+                setDrivePower(turn.AngleChange(thetaG, thetaA));
+            }
+        }
+        while(!goalReachedAngle);
+        setDrivePower(new double[] {0,0,0,0});
     }
     
-    public class Movement {
-        public static class Moving {
-            
-            public static double[] AbsolutePosition(double theta) {
+     public class Moving {
+
+            public double[] AbsolutePosition(double theta) {
                 robotPosition.setHeading(theta);
                 double[] PreviousPosition = robotPosition.getPosition();
                 double[] CurrentPosition = new double[2];
                 double[] XEncoderPosition = new double[2];
                 double[] YEncoderPosition = new double[2];
-                double ConvRate = (PI * 90) / 208076.8; // Change this
+                double ConvRate = 0.006184246955207152778; // Change this
                 double[] WeirdOrlandoMathsX = {sin(theta), cos(theta)};
                 double[] WeirdOrlandoMathsY = {cos(theta), sin(theta)};
                 //Get bulk data from hub
@@ -88,9 +107,9 @@ public class CROdometery {
                 syncEncoders();
                 robotPosition.setPosition(CurrentPosition[0], CurrentPosition[1]);
                 return CurrentPosition;
-            }*/
+            }
 
-       /*     public static double[] PositionChange(double Xg, double Xa, double Yg, double Ya) {
+            public double[] PositionChange(double Xg, double Xa, double Yg, double Ya) {
                 float[] MoveYBasePower = {0.55f, 1.0f, 0.5f, 1.0f};                             //Base Motor Power for Y movement
                 float[] MoveXBasePower = {0.4f, -0.95f, -0.45f, 0.95f};                            //Base Motor Power for X movement
                 Yg -= Ya;
@@ -107,9 +126,9 @@ public class CROdometery {
                 }
                 return motorPower;
             }
-            
-            public static boolean GoalCheckPos(double Xa, double Xg, double Ya, double Yg) { /**/
-       /*
+
+            public boolean GoalCheckPos(double Xa, double Xg, double Ya, double Yg) { /**/
+
                 boolean reachedGoal = false;
                 if(abs(Xg - Xa) < 1) {
                     reachedGoal = true;
@@ -122,12 +141,39 @@ public class CROdometery {
                 }
                 return reachedGoal;
             }
-            
+
         }
-        public static class Turning {
-            
-            public static double[] AngleChange(double thetaG, double thetaA) { /**/
-           /*     float[] TurnBasePower = {0.65f, 1.0f, -0.65f, -1.0f};
+        public class Turning {
+            /** Note: wheelDelta - angleChange * wheelOffset */
+            public double[] TurningPositon(double theta, double previous){
+                    robotPosition.setHeading(theta);
+                    double[] PreviousPosition = robotPosition.getPosition();
+                    double[] CurrentPosition = new double[2];
+                    double[] XEncoderPosition = new double[2];
+                    double[] YEncoderPosition = new double[2];
+                    double ConvRate = 0.006184246955207152778; // Change this
+                    double[] WeirdOrlandoMathsX = {sin(theta), cos(theta)};
+                    double[] WeirdOrlandoMathsY = {cos(theta), sin(theta)};
+                    //Get bulk data from hub
+                    bulkData = expansionHubEx.getBulkInputData();
+                    //get x encoder
+                    int XClicks =  (int)(x_encoder.getCounts(bulkData) - ((theta - previous) * 1));
+                    //Get the y encoders
+                    int YClicks = ((left_y_encoder.getCounts(bulkData) + right_y_encoder.getCounts(bulkData)) / 2);
+                    for(int i = 0; i < 2; i++) {
+                        XEncoderPosition[i] = ConvRate * XClicks * WeirdOrlandoMathsX[i];
+                        YEncoderPosition[i] = ConvRate * YClicks * WeirdOrlandoMathsY[i];
+                    }
+                    for(int k = 0; k < 2; k++) {
+                        CurrentPosition[k] = XEncoderPosition[k] + YEncoderPosition[k] + PreviousPosition[k];
+                    }
+                    syncEncoders();
+                    robotPosition.setPosition(CurrentPosition[0], CurrentPosition[1]);
+                    return CurrentPosition;
+            }
+
+            public double[] AngleChange(double thetaG, double thetaA) { /**/
+                float[] TurnBasePower = {0.65f, 1.0f, -0.65f, -1.0f};
                 thetaA -= thetaG;
                 double[] motorPower = new double[4];
                 for(int i = 0; i < 4; i++) {
@@ -135,18 +181,22 @@ public class CROdometery {
                 }
                 return motorPower;
             }
-            public static boolean GoalCheckAngle(double thetaG, double thetaA) { /**/
-           /*     boolean reachedGoal = false;
+            public boolean GoalCheckAngle(double thetaG, double thetaA) { /**/
+                boolean reachedGoal = false;
                 double thetaDif = (thetaG - thetaA);
-                if(abs(thetaDif) < 1) {
+                if (abs(thetaDif) < 1) {
                     reachedGoal = true;
                 }
                 return reachedGoal;
             }
-        }
-    }*/
+    }
 
-    
+    private void setDrivePower(double[] powers) {
+        left_front_drive.setPower(powers[0]);
+        left_back_drive.setPower(powers[1]);
+        right_front_drive.setPower(powers[2]);
+        right_back_drive.setPower(powers[3]);
+    }
     
     
     public void syncEncoders(){
