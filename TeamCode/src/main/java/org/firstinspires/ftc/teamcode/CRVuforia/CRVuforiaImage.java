458aa8a8adbfa03e133171f350b429f5310e5b5d
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2019-12-02 21:16:54 -0500

    asfsda

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CRVuforia/CRVuforiaImage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CRVuforia/CRVuforiaImage.java
@@ -4,51 +4,55 @@ import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+
+import com.vuforia.Matrix44F;
 import com.vuforia.TrackableResult;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
+import com.vuforia.Tool;
 
 
-public class CRVuforiaImage extends VuforiaTrackableDefaultListener implements VuforiaTrackable.Listener{
+public class CRVuforiaImage implements VuforiaTrackable.Listener{
 
   OpenGLMatrix stoneLocation;
   VectorF stoneTranslation;
+  VuforiaTrackable trackable;
+  boolean targetVisible;
   float x;
   float y;
   float z;
 
   public CRVuforiaImage(VuforiaTrackable trackable){
-    super(trackable);
+    this.trackable = trackable;
   }
   
   protected VectorF findImagePos(){
-    stoneLocation = super.getVuforiaCameraFromTarget();
     try {
       stoneTranslation = stoneLocation.getTranslation();
     } catch (NullPointerException e){
       return null;
     }
-    x = stoneTranslation.get(0);
-    y = stoneTranslation.get(1);
-    z = stoneTranslation.get(2);
     return stoneTranslation;
   }
 
-  protected boolean imageVisible(){ return super.isVisible();}
+  protected boolean imageVisible(){
+    return targetVisible;
+  }
 
   @Override
   public void onTracked(TrackableResult trackableResult, CameraName cameraName, Camera camera, VuforiaTrackable child){
-    super.onTracked(trackableResult, cameraName, camera, child);
+    targetVisible = true;
+    stoneLocation = new OpenGLMatrix(Tool.convertPose2GLMatrix(trackableResult.getPose()));
   }
 
   @Override
   public void onNotTracked(){
-    super.onNotTracked();
+    targetVisible = false;
   }
 
   @Override
   public void addTrackable(VuforiaTrackable trackable){
-    super.addTrackable(trackable);
+    this.trackable = trackable;
   }
 
 
