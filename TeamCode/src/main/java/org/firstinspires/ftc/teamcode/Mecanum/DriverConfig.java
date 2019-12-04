274ab96e951f2185bb8db9565a01d0476195e398
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2019-12-04 09:57:09 -0500

    Dbbdhc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/DriverConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/DriverConfig.java
@@ -32,6 +32,10 @@ public class DriverConfig {
         START_BUTTON,
         GUIDE_BUTTON
     }
+    public enum Claw controls {
+        TRIGGERS,
+        BUMPERS
+    }
 
 
 
@@ -68,10 +72,39 @@ public class DriverConfig {
 
     /**Configuration for manipulator */
     public static class Manipulator {
-        public boolean f;
+        public LiftMode liftMode;
+        public DirectionalControls liftControls, armControls;
+        public Button liftSlowModeButton;
+        public ClawControls clawControls;
 
         public Manipulator(List<Element> manipulatorList){
-
+            liftMode = (manipulatorList.get(0).getText().equals("smooth")) ? LiftMode.SMOOTH : LiftMode.STAGED;
+            switch (manipulatorList.get(1).getText()){
+                 case "dpad": liftControls = DirectionalControls.DPAD;
+                 case "left_stick": liftControls = DirectionalControls.LEFT_STICK;
+                 case "right_stick": liftControls = DirectionalControls.RIGHT_STICK;
+            }
+            switch(manipulatorList.get(2).getText()){
+                case "left_bumper": liftSlowModeButton = Button.LEFT_BUMPER;
+                case "right_bumper": liftSlowModeButton = Button.RIGHT_BUMPER;
+                case "x": liftSlowModeButton = Button.X;
+                case "y": liftSlowModeButton = Button.Y;
+                case "a": liftSlowModeButton = Button.A;
+                case "b": liftSlowModeButton = Button.B;
+                case "left_stick_button": liftSlowModeButton = Button.LEFT_STICK_BUTTON;
+                case "right_stick_button": liftSlowModeButton = Button.RIGHT_STICK_BUTTON;
+            }
+            switch (manipulatorList.get(3).getText()){
+                 case "dpad": armControls = DirectionalControls.DPAD;
+                 case "left_stick": armControls = DirectionalControls.LEFT_STICK;
+                 case "right_stick": armControls = DirectionalControls.RIGHT_STICK;
+            }
+             switch (manipulatorList.get(4).getText()){
+                 case "triggers": armControls = ClawControls.TRIGGERS;
+                 case "bumpers": armControls = ClawControls.BUMPERS;
+            } 
+            
+            
         }
     }
 }
