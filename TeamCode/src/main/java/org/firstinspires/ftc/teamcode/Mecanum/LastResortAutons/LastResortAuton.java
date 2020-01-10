94c809501769e75cfc723ed0e181b017ff50dfb4
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2020-01-09 21:42:16 -0500

    Time based things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/LastResortAutons/LastResortAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/LastResortAutons/LastResortAuton.java
@@ -1,13 +1,20 @@
 package org.firstinspires.ftc.teamcode.Mecanum.LastResortAutons;
-
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.CRVuforia.Vuforia;
 import org.firstinspires.ftc.teamcode.Methods.GeneralMethods;
+import org.firstinspires.ftc.teamcode.hardware.DriveBaseVectors;
 
 @Autonomous(name="Last Resort BlueLoadingZone", group = "Last Resort")
 public class LastResortAuton extends LinearOpMode {
@@ -24,13 +31,178 @@ public class LastResortAuton extends LinearOpMode {
     Servo claw = null;
     public WebcamName webcam = null;
 
-    //public BNO055IMU imu;
-    //private Orientation angles;
+    private BNO055IMU imu;
+    private Orientation angles;
 
+
+    private static final double TURNING_ANGLE_P = 2;
+    private static final double HOLDING_ANGLE_P = 2.5;
+    private static final double TURNING_ANGLE_THRESHOLD = 1;
+
+
+    private void stopDrive(){
+        left_front_drive.setPower(0);
+        left_back_drive.setPower(0);
+        right_front_drive.setPower(0);
+        right_back_drive.setPower(0);
+    }
+
+
+    /*So distance between the current angle and target angle is normalized to the range of -180 to 180, and then that value is passed to a p controller. */
+
+    private void turnTo(double targetAngle, double speed){
+
+        double error;
+        double output;
+        error = getError(targetAngle);
+        output = getPOutput(error, TURNING_ANGLE_P);
+
+        if(opModeIsActive()) {
+
+            while (opModeIsActive() && (error >= TURNING_ANGLE_THRESHOLD)) {
+                error = getError(targetAngle);
+                output = getPOutput(error, TURNING_ANGLE_P);
+
+                left_front_drive.setPower(DriveBaseVectors.turnCW[0] * output * speed);
+                left_back_drive.setPower(DriveBaseVectors.turnCW[1] * output * speed);
+                right_front_drive.setPower(DriveBaseVectors.turnCW[2] * output * speed);
+                right_back_drive.setPower(DriveBaseVectors.turnCW[3] * output * speed);
+            }
+        }
+
+    }
+
+    /**
+     * Controls power to stay at a designated heading (must be a 90 degree increment) and
+     * also will stop if the opMode ends or the time runs out.
+     *
+     * Need to set the power before calling this
+     *
+     *
+     *
+     * Angle options:
+     *
+     * Straight Forward = 0
+     * Strafe Left = negative 90
+     * Strafe Right = positive 90
+     * Backwards (may be very unstable, use cautiously) = somewhere from -175 to 175
+     *
+     * @param speed the overall speed of the movement
+     * @param angle The angle to hold
+     * @param timeInMillis The time to run for
+     * @param relativeLeftMotors Which two motors control the left side of the robot
+     * @param relativeRightMotors Which two motors control the right side of the robot
+     */
+    private void holdAngle(double speed, double timeInMillis, double angle,
+                                  DcMotor[] relativeLeftMotors, DcMotor[] relativeRightMotors){
+
+        if(opModeIsActive()) {
+            // keep looping while we are still active, and BOTH motors are running.
+
+            ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
+            while (opModeIsActive() && (timer.milliseconds() <= timeInMillis)) {
+
+                // adjust relative speed based on heading error.
+                double error = getError(angle);
+                double steer = getPOutput(error, HOLDING_ANGLE_P);
+                double leftSpeed;
+                double rightSpeed;
+
+
+                leftSpeed = speed - steer;
+                rightSpeed = speed + steer;
+
+
+                // Normalize speeds if either one exceeds +/- 1.0;
+                double max = Math.max(Math.abs(leftSpeed), Math.abs(rightSpeed));
+                if (max > 1.0) {
+                    leftSpeed /= max;
+                    rightSpeed /= max;
+                }
+
+                for (DcMotor motor : relativeLeftMotors) {
+                    motor.setPower(motor.getPower() * leftSpeed);
+                }
+
+                for (DcMotor motor : relativeRightMotors) {
+                    motor.setPower(motor.getPower() * rightSpeed);
+                }
+
+            }
+        }
+
+        stopDrive();
+    }
+    /**
+     * Returns the relative error between the current angle and the target angle
+     * Target angle must be within the -180/+180 range.
+     * NOTE: DO NOT ASK FOR 180 or -180 degrees everything breaks
+     * @param targetAngle Target Angle within -180/+180 range
+     * @return The error normalized to -180/+180
+     */
+    private double getError(double targetAngle) {
+
+        // calculate error in -179 to +180 range  (
+        double robotError = targetAngle - degreesConversion();
+        return AngleUnit.normalizeDegrees(robotError);
+    }
+
+    private double getPOutput(double error, double PCoefficient){
+        return Range.clip(error * PCoefficient, -1, 1);
+    }
+
+    private double degreesConversion(){
+        double theta = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        theta = AngleUnit.normalizeDegrees(theta + 90);
+        return theta;
+    }
+
+
+    /**
+     * Nelitha here is a sample usage of the above controls
+     *
+     * <br>
+     *     <br>
+     *
+     *
+     * Say I want to strafe directly left from my current heading for 900 milliseconds
+     *
+     * Set the default power:
+     *
+     * <br>
+     * {@code
+     * left_front_drive.setPower(0.6);
+     * left_back_drive.setPower(-0.9);
+     * right_front_drive.setPower(-0.6);
+     * right_back_drive.setPower(0.9);
+     * }
+     *
+     * <br>
+     *     <br>
+     * Then hand over control to the controller with a speed of 0.9, a time of 900, an
+     * angle of 90 degrees, and the back two motors as the "left" and the front two as the "right".
+     * <br>
+     *     <br>
+     *
+     * {@code holdAngle(0.9, 900, 90, new DcMotor[] {left_back_drive, right_back_drive},
+     * new DcMotor[] {left_front_drive, right_front_drive}); }
+     *
+     *
+     * <br>
+     *     <br>
+     *
+     * For turning, no previous power setting is needed, just do
+     *
+     * {@code turnTo(targetAngle, speed) }
+     *
+     *
+     *
+     *
+     */
     @Override
     public void runOpMode() {
 
-        blockPosBlue.startVision();
 
 
         left_front_drive = hardwareMap.get(DcMotor.class, "left_front_drive");
@@ -41,11 +213,27 @@ public class LastResortAuton extends LinearOpMode {
         webcam = hardwareMap.get(WebcamName.class, "Webcam 1");
         claw = hardwareMap.get(Servo.class, "claw");
 
-        //blockPosBlue.blueInit(webcam);
+        blockPosBlue.blueInit(webcam, this);
 
 
         left_front_drive.setDirection(DcMotor.Direction.REVERSE);
         left_back_drive.setDirection(DcMotor.Direction.REVERSE);
+
+
+        // MORE IMU STUFF
+
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu 1".
+        imu = hardwareMap.get(BNO055IMU.class, "imu 1");
+        imu.initialize(parameters);
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
 
 
 /*
@@ -60,13 +248,11 @@ to strafe (right):
         waitForStart();
 
 
-        left_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        left_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        right_front_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        right_back_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left_front_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        left_back_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        right_front_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        right_back_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-
-        TimeBasedRunner runner = new TimeBasedRunner(0, this);
 
 
 
@@ -218,12 +404,13 @@ to strafe (right):
 
         while(opModeIsActive() && new ElapsedTime().milliseconds() >= 5){
             telemetry.update();
+            blockPosBlue.updateVision();
         }
         blockPosBlue.stopVision();
 
         Vuforia.SkystonePosition pose = blockPosBlue.getResults();
 
-        switch(pose){
+        switch(pose) {
             case LEFT://left
                 left_front_drive.setPower(0.6);
                 left_back_drive.setPower(-0.9);
@@ -239,12 +426,12 @@ to strafe (right):
 
                 sleep(500);
 
-                            left_front_drive.setPower(0.3);
-                            left_back_drive.setPower(0.3);
-                            right_front_drive.setPower(0.3);
-                            right_back_drive.setPower(0.3);
+                left_front_drive.setPower(0.3);
+                left_back_drive.setPower(0.3);
+                right_front_drive.setPower(0.3);
+                right_back_drive.setPower(0.3);
 
-                            sleep(1000);
+                sleep(1000);
 
 
                 left_front_drive.setPower(0);
