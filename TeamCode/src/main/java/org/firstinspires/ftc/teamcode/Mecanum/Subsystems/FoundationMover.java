9881017c99353ad0e0ef242c11d00e6b11946860
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2020-01-10 12:17:05 -0500

    Servo stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/Subsystems/FoundationMover.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/Subsystems/FoundationMover.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Mecanum.Subsystems;
 
 
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import kotlin.Unit;
@@ -9,35 +10,41 @@ import kotlin.jvm.functions.Function0;
 
 public class FoundationMover implements Subsystem {
 
-
-    private CRServo left = null;
-    private CRServo right = null;
+    
+    private Servo left = null;
+    private Servo right = null;
 
 
     public FoundationMover(HardwareMap hardwareMap){
-        left = hardwareMap.get(CRServo.class, "left_mover");
-        right = hardwareMap.get(CRServo.class, "right_mover");
+        left = hardwareMap.get(Servo.class, "left_foundation_mover");
+        right = hardwareMap.get(Servo.class, "right_foundation_mover");
     }
 
     @Override
     public void init(){
-        right.setDirection(CRServo.Direction.REVERSE);
-        byPower(0);
+        //You can now set the position as if it was the opposite way.
+        right.setDirection(Servo.Direction.REVERSE);
+        down();
     }
 
 
 
     public void down(){
-        byPower(-0.2);
+        left.setPosition(0);
+        right.setPosition(0);
     }
 
     public void up(){
-        byPower(0.2);
+        left.setPosition(1);
+        right.setPosition(1);
     }
 
-    public void stop(){
-        byPower(0);
+    public void close(){
+        left.setPosition(0.2);
+        right.setPosition(0.2);
     }
+    
+    
 
     public void byPower(double power){
         left.setPower(power);
