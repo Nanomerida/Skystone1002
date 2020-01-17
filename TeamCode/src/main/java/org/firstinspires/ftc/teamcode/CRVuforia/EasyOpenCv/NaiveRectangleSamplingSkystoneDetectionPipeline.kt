e94eb84757b2b1b3664dc28e32fe8dadc1c619ad
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2020-01-17 07:28:45 -0500

    OpenCv Pipeline
    Added things to the pipeline, including a Text above the found position, and section dividers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CRVuforia/EasyOpenCv/NaiveRectangleSamplingSkystoneDetectionPipeline.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CRVuforia/EasyOpenCv/NaiveRectangleSamplingSkystoneDetectionPipeline.kt
@@ -8,14 +8,19 @@ class NaiveRectangleSamplingSkystoneDetectionPipeline : Init3BlockDetection() {
     private val matYCrCb = Mat()
     private val matCb = Mat()
 
-    private val samplingRectWidth = 15
-    private val samplingRectHeight = 15
-    private val samplingRectColor = Scalar(0.0, 0.0, 255.0)
+    private val samplingRectWidth = 17
+    private val samplingRectHeight = 17
+    private val samplingRectColor = Scalar(0.0, 255.0, 0.0)
     private val samplingRectThickness = 1
 
     private val samplingCircleRadius = 5
     private val samplingCircleColor = Scalar(225.0, 52.0, 235.0)
     private val samplingCircleThickness = -1
+
+    private val sampleSectionDivLength = 60
+    private val sampleSectionDivColor = Scalar(00.0, 255.0, 40.0)
+    private val sampleSectionDivThickness = 4
+    private val sectionDivHalfLenght = sampleSectionDivLength / 2
 
     private val samplePointPercentages = arrayOf(
             arrayOf(.18, .59), arrayOf(.5, .59), arrayOf(.82, .59)
@@ -35,21 +40,24 @@ class NaiveRectangleSamplingSkystoneDetectionPipeline : Init3BlockDetection() {
     }
 
     override fun processFrame(input: Mat): Mat {
-        // Convert the image from RGB to YCrCb
+
+        // Convert the image from RGB to
         Imgproc.cvtColor(input, matYCrCb, Imgproc.COLOR_RGB2YCrCb)
 
         // Extract the Cb channel from the image
         Core.extractChannel(matYCrCb, matCb, 2)
 
-        // The the sample areas from the Cb channel
+        // Then the sample areas from the Cb channel
         var subMats = samplePoints.map {
             matCb.submat(Rect(it[0], it[1]))
         }
+
 
         // Average the sample areas
         val avgSamples = subMats.map {
             Core.mean(it).`val`[0]
         }
+
 
         // Draw rectangles around the sample areas
         samplePoints.forEach {
@@ -73,6 +81,55 @@ class NaiveRectangleSamplingSkystoneDetectionPipeline : Init3BlockDetection() {
                 samplingCircleColor,
                 samplingCircleThickness
         )
+
+        //Add Text over the detected position
+        Imgproc.putText(
+                input,
+                "FOUND!",
+                Point(
+                        ((detectedPoint[0].x + detectedPoint[1].x) / 2) - 50,
+                        ((detectedPoint[0].y + detectedPoint[1].y) / 2) - 40
+                ),
+                Imgproc.FONT_HERSHEY_PLAIN,
+                1.5,
+                Scalar(0.00, 255.0, 0.0),
+                3
+
+        )
+
+        //Add section dividers for clarity
+
+        Imgproc.line(
+                input,
+                Point(
+                        width * 0.333333,
+                        height - 20.0
+                ),
+                Point(
+                        width * 0.33333333,
+                        20.0
+                ),
+                sampleSectionDivColor,
+                sampleSectionDivThickness
+        )
+
+        Imgproc.line(
+                input,
+                Point(
+                        width * 0.6666666,
+                        height - 20.0
+                ),
+                Point(
+                        width * 0.66666666,
+                        20.0
+                ),
+                sampleSectionDivColor,
+                sampleSectionDivThickness
+        )
+
+
+
+
 
         // Free the allocated submat memory
         subMats.forEach {
